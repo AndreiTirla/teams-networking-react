541149d6a55935d1a18892076b829a2aac736ade
Andrei Tirla <andreitirla16@gmail.com>
2021-03-15 21:40:27 +0200

    added PersonsTable

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { PersonsTable } from "./PersonsTable";
+
+let persons = [
+  {
+    id: "a123",
+    firstName: "Tirla",
+    lastName: "Andrei",
+    gitHub: "AndreiTirla",
+  },
+  {
+    id: "b654",
+    firstName: "Nicolae",
+    lastName: "Matei",
+    gitHub: "nmatei",
+  },
+];
 
 function App() {
   return (
     <div>
       <h1>Teams Networking</h1>
       <div>Search</div>
-      <p>table...</p>
+      <PersonsTable persons={persons} />
     </div>
   );
 }
